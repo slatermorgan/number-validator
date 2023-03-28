664e49f3b5bd5df91967c17ba271de07e7ed2953
Morgan Slater <morgan.slater@oneiota.co.uk>
2023-03-28 17:37:36 +0100

    improve function readablity

--- main.go
+++ main.go
@@ -7,28 +7,30 @@ import (
 // weightingFactor is a lookup for what to multiply each digit by
 var weightingFactor = [9]int{10, 9, 8, 7, 6, 5, 4, 3, 2}
 
-// checkDigitIndex is the index of the check digit in the number string
-const checkDigitIndex = 9
+const expectedNumberLength = 10
 
 // divisor is used to divide the total by (11 for the mod11 algorithm)
 const divisor = 11
 
+// The check digit value if a result of 11 is found.
+const elevenCheckDigit = 0
+
 // IsValidNumber checks if a number passes the mod11 algorithm.
 // arguement must be a 10 digit number
-func IsValidNumber(numString string) bool {
-	if len(numString) != 10 {
+func IsValidNumber(input string) bool {
+	if len(input) != expectedNumberLength {
 		return false
 	}
 
 	var total int
 	var lastDigit int
-	for k, char := range numString {
+	for k, char := range input {
 		digit, err := strconv.Atoi(string(char))
 		if err != nil {
 			return false
 		}
 
-		if k == checkDigitIndex {
+		if k == expectedNumberLength-1 {
 			lastDigit = digit
 
 			break
@@ -43,7 +45,7 @@ func IsValidNumber(numString string) bool {
 
 	// If the result is 11 then a check digit of 0 is used.
 	if checkDigit == 11 {
-		return lastDigit == 0
+		return lastDigit == elevenCheckDigit
 	}
 
 	return lastDigit == checkDigit
