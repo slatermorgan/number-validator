29e020a9d6145410006d0f8e1765bcf9c4c7236e
Morgan Slater <morgan.slater@oneiota.co.uk>
2023-03-28 17:00:42 +0100

    write initial solution

--- main.go
+++ main.go
@@ -1,5 +1,41 @@
 package main
 
-func IsValidNumber(num string) bool {
-	return false
+import (
+	"strconv"
+)
+
+var weightingFactor = []int{10, 9, 8, 7, 6, 5, 4, 3, 2}
+
+const checkDigitIndex = 9
+
+const divisor = 11
+
+func IsValidNumber(numString string) bool {
+	if len(numString) != 10 {
+		return false
+	}
+
+	var total int
+	var lastDigit int
+	for k, char := range numString {
+		digit := string(char)
+		num, err := strconv.Atoi(digit)
+		if err != nil {
+			return false
+		}
+
+		if k == checkDigitIndex {
+			lastDigit = num
+
+			break
+		}
+
+		total += num * weightingFactor[k]
+	}
+
+	remainder := total % divisor
+
+	checkDigit := divisor - remainder
+
+	return lastDigit == checkDigit
 }
